374a7975196ac2f5b6181a9096b319986a11af68
jmolinaso <jmolinaso@391858f3-ae84-438e-8215-6154f2daa8c2>
2011-07-30 14:27:36 +0000

    Added license information for app eyegraphs

--- branches/jmolinaso/eyeos/eyeos/apps/eyegraphs/eyegraphs.js
+++ branches/jmolinaso/eyeos/eyeos/apps/eyegraphs/eyegraphs.js
@@ -1,3 +1,41 @@
+/*
+*                 eyeos - The Open Source Cloud's Web Desktop
+*                               Version 2.0
+*                   Copyright (C) 2007 - 2010 eyeos Team 
+*                   
+*                   eyeGraphs - Application for graphing in eyeos
+*                   			Version 1.0
+*                   Copyleft by Julio Molina Soler <jmolinaso@gmail.com>
+* 
+* This application is developed by Julio Molina Soler and it's ment to be distributed
+* with eyeos 2.x version based on qooxdoo 1.4 and qooxdoo-contribs. 
+* It uses the qxdygraphs and is distributed under LGPL/EPL by Tobi Oetiker.
+* 
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Affero General Public License version 3 as published by the
+* Free Software Foundation.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+* details.
+* 
+* You should have received a copy of the GNU Affero General Public License
+* version 3 along with this program in the file "LICENSE".  If not, see 
+* <http://www.gnu.org/licenses/agpl-3.0.txt>.
+* 
+* See www.eyeos.org for more details. All requests should be sent to licensing@eyeos.org
+* 
+* The interactive user interfaces in modified source and object code versions
+* of this program must display Appropriate Legal Notices, as required under
+* Section 5 of the GNU Affero General Public License version 3.
+* 
+* In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+* these Appropriate Legal Notices must retain the display of the "Powered by
+* eyeos" logo and retain the original copyright notice. If the display of the 
+* logo is not reasonably feasible for technical reasons, the Appropriate Legal Notices
+* must display the words "Powered by eyeos" and retain the original copyright notice. 
+*/
 function eyegraphs_application(checknum,pid,args){
 	var app = new eyeos.application.eyeGraphs(checknum,pid);
 	app.open();
@@ -99,9 +137,7 @@ qx.Class.define("eyeos.application.eyeGraphs",
 			    	}, this);
 			    				    },
 			 __menu_bar: function(){
-					// TOOLBAR
-			    	//
-			    	var toolbarBox = new qx.ui.toolbar.ToolBar();
+					var toolbarBox = new qx.ui.toolbar.ToolBar();
 			    	
 			    	var openButton = new qx.ui.toolbar.Button(tr('Open csv file'), 'index.php?extern=images/16x16/actions/document-open.png');
 			    	openButton.addListener("execute", function(){
@@ -129,7 +165,6 @@ qx.Class.define("eyeos.application.eyeGraphs",
 				 this.__init_plot(this.__data,this.__options);
 				 _window.add(this.__menu_bar());
 				 _window.add(this.__right_pane());
-				 //mainWindow
 				 _window.open();
 				 
 			 }
